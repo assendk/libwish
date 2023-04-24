7672cd56fd2fe3091d568c5fd25bd00245c82e1c
assendk <assendk@gmail.com>
2023-04-24 13:04:09 +0300

    updates. trying to fix the login

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -31,6 +31,14 @@ class SecurityController extends AbstractController
      */
     public function logout(): void
     {
-        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+        // Clear authentication token from session
+        $this->get('security.token_storage')->setToken(null);
+
+        // Invalidate the session
+        $this->get('session')->invalidate();
+
+        // Redirect to the homepage or login page after logout
+        $this->redirectToRoute('homepage');
+//        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
