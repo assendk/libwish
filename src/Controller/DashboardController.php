870324d5bd9e48de86889f54c9795990ba30330a
assendk <assendk@gmail.com>
2023-04-24 12:10:17 +0300

    updates

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -9,10 +9,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class DashboardController extends AbstractController
 {
     /**
-     * @Route("/dashboard", name="app_dashboard")
+     * @Route("/dashboard", name="dashboard")
      */
     public function index(): Response
     {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
         return $this->render('dashboard/index.html.twig', [
             'controller_name' => 'DashboardController',
         ]);
